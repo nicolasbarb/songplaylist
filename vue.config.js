c9925696e83173a361ab424162a986532247c07d
Nicolas Barbosa <nicolas.barbosa@orange.com>
2020-11-11 11:53:20 +0100

    Updated app to be used as a pwa application

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
-}
+  ],
+  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+
+  }
+};
